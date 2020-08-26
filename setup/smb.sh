3f775d9d45bc04d692d463d825378979b7b2c0e9
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-08-25 19:30:13 -0500

    Create smb.sh

---
+++ setup/smb.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# CIFS support ([article](https://wiki.ubuntu.com/MountWindowsSharesPermanently))
+# Use this for anything that has SMB enabled to avoid permission issues in certain situations
+
+sudo apt update
+sudo apt install -y cifs-utils
+
+sh -c 'cat > ~/.smbcredentials << EOF
+username=docker
+password=mspassword
+domain=
+EOF'
+chmod 600 ~/.smbcredentials
+echo "Run 'vi ~/.smbcredentials' and edit the password"
+
+sudo mkdir -p /mnt/nas/data1/media
+echo "//nas/media /mnt/nas/data1/media cifs rw,uid=docker,gid=asyla,credentials=/home/`id -un`/.smbcredentials 0 0" | sudo tee --append /etc/fstab
+
+sudo mount -a
